a98ecabbd0aed8eb2aee85e1ad99ac304e1e046c
Ricardo <r2volk@gmail.com>
2026-02-21 00:16:21 -0500

    ultimo commit

--- lib/data/model/superhero_detail_response.dart
+++ lib/data/model/superhero_detail_response.dart
@@ -2,10 +2,17 @@
 class SuperheroDetailResponse {
   final String id;
   final String name;
+  final String url;
 
-  SuperheroDetailResponse({required this.id, required this.name});
+  SuperheroDetailResponse({
+    required this.id,
+    required this.name,
+    required this.url});
   
   factory SuperheroDetailResponse.fromJson(Map<String, dynamic> json){
-    return SuperheroDetailResponse(id: json["id"], name: json["name"]);
+    return SuperheroDetailResponse(
+      id: json["id"],
+      name: json["name"],
+      url: json["image"]["url"]);
   }
 }
--- lib/screens/superhero_search_screen.dart
+++ lib/screens/superhero_search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:superhero_app/data/model/superhero_detail_response.dart';
 import 'package:superhero_app/data/model/superhero_response.dart';
 import 'package:superhero_app/data/repository.dart';
 
@@ -35,32 +36,81 @@ class _SuperheroSearchScreenState extends State<SuperheroSearchScreen> {
               },
             ),
           ),
-          FutureBuilder(future: _superheroInfo, builder: (context,snapshot){
-            if(snapshot.connectionState == ConnectionState.waiting){
-              return CircularProgressIndicator();
-            }else if (snapshot.hasError){
-              return Text("Error: ${snapshot.error}");
-            }else if(snapshot.hasData){
-              var superheroList = snapshot.data?.result;
-              return SizedBox(
-                height: 300,
-                child: ListView.builder(
-                  itemCount: superheroList?.length ?? 0,
-                  itemBuilder: (context,index){
-                    if (superheroList != null){
-                      return Text(superheroList[index].name);
-                    }else{
-                      return Text("Error");
-                    }
-                  },
-                ),
-              );
-            }else{
-              return Text("No hay resultados");
-            }
-          })        
+          bodyList()        
         ],
       ),
     );
   }
+
+  FutureBuilder<SuperheroResponse?> bodyList() {
+    return FutureBuilder(future: _superheroInfo, builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return CircularProgressIndicator();
+          }else if (snapshot.hasError){
+            return Text("Error: ${snapshot.error}");
+          }else if(snapshot.hasData){
+            var superheroList = snapshot.data?.result;
+            return Expanded(
+              child: ListView.builder(
+                itemCount: superheroList?.length ?? 0,
+                itemBuilder: (context,index){
+                  if (superheroList != null){
+                    return itemSuperhero(superheroList[index]);
+                  }else{
+                    return Text("Error");
+                  }
+                },
+              ),
+            );
+          }else{
+            return Text("No hay resultados");
+          }
+        });
+  }
+
+  Padding itemSuperhero(SuperheroDetailResponse item) => Padding(
+    padding: const EdgeInsets.all(32),
+    child: Container(
+  // Le dice al Container que recorte (clippee) a sus hijos
+  // usando la forma definida en la decoración (en este caso, el borderRadius)
+  clipBehavior: Clip.hardEdge,
+
+  decoration: BoxDecoration(
+    // Define bordes redondeados de 16 píxeles en las 4 esquinas
+    borderRadius: BorderRadius.circular(16),
+
+    // Color de fondo del Container
+    color: Colors.red,
+  ),
+
+  // Widget hijo del Container
+  child: Column(
+    // Organiza los hijos en vertical (uno debajo del otro)
+    children: [
+
+      Image.network(
+        // URL de la imagen que se carga desde internet
+        item.url,
+
+        // Altura fija de la imagen
+        height: 250,
+
+        // Ocupa todo el ancho disponible del Container
+        width: double.infinity,
+
+        // Hace que la imagen llene todo el espacio manteniendo proporción
+        // Si sobra imagen, la recorta
+        fit: BoxFit.cover,
+
+        // Ajusta qué parte de la imagen se prioriza al recortar
+        // (0, -0.6) significa centrado horizontal y un poco hacia arriba verticalmente
+        alignment: Alignment(0, -0.6),
+      ),
+
+      // Muestra el nombre del superhéroe debajo de la imagen
+      Text(item.name),
+    ],
+  ),
+),
+  );
 }
